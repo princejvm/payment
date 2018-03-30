c37af806461d72a8c698a3860b73c82ecb04a8b8
princejvm <sksinha.c@gmail.com>
2018-03-30 15:22:26 +0530

    payment work

---
+++ PaymentJava/src/MailDebug.java
@@ -0,0 +1,7 @@
+
+public class MailDebug {
+	public static void main(String args[]) {
+	      TestMail.send("perseverancenitj@gmail.com","perseverance@nitj","princekumarjvmnitj@gmail.com","e-challan","hello");  
+
+	}
+}
