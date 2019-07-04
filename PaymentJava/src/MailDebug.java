72cea1e15920210a918adfcad816a1257d998033
PRINCE KUMAR <sksinha.c@gmail.com>
2019-07-04 20:08:10 +0530

    Delete MailDebug.java

--- PaymentJava/src/MailDebug.java
+++
@@ -1,7 +0,0 @@
-
-public class MailDebug {
-	public static void main(String args[]) {
-	      TestMail.send("perseverancenitj@gmail.com","perseverance@nitj","princekumarjvmnitj@gmail.com","e-challan","hello");  
-
-	}
-}
